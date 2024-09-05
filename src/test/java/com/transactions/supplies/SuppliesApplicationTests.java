b28fdc1fa517181134f44bc5f7f8242835784b45
byLugos <bylugos147@gmail.com>
2024-09-05 17:10:16 -0500

    Initial config

---
+++ src/test/java/com/transactions/supplies/SuppliesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.transactions.supplies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuppliesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
